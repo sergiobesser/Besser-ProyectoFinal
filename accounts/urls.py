8eda1528afb16f380a97e8858d77a4207f6b8fd7
Sergio Besser <sergiobesser@hotmail.com>
2022-04-02 19:08:55 -0300

    Se creo registro de usuarios

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
+import django
 from django.urls import path
-from .views import accounts, login, login_view
+from .views import accounts, login, login_view, signup, signup_view
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', accounts, name='accounts'),
-    path('loginview', login_view, name='login_view'),
+    path('loginview/', login_view, name='login_view'),
     path('login/', login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('signupview/', signup_view, name='signup_view'),
+    path('signup', signup, name='signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as django_login, authenticate
+from .forms import MyUserForm
+
 
 def accounts(request):
     return render(request, 'accounts/accounts.html', {})
 
+
 def login_view(request):
     return render(request, 'accounts/login_view.html', {})
+
 
 def login(request):
     msj = ''
@@ -28,3 +32,23 @@ def login(request):
     
     login_form = AuthenticationForm()
     return render(request, 'accounts/login_form.html', {'login_form': login_form, 'msj': msj})
+
+
+def signup_view(request):
+    return render(request, 'accounts/signup_view.html', {})
+
+
+def signup(request):
+    
+    if request.method == 'POST':
+        signup_form = MyUserForm(request.POST)
+        
+        if signup_form.is_valid():
+            username = signup_form.cleaned_data['username']
+            signup_form.save()
+            return render(request, 'accounts/signup_view.html')
+        else:
+            return render(request, 'accounts/signup_form.html', {'signup_form': signup_form, 'msj': 'El formulario no es valido'})
+        
+    signup_form = MyUserForm()
+    return render(request, 'accounts/signup_form.html', {'signup_form': signup_form})
