d950921278f82840bf5f13ca9690ae6c1f81bad3
Sergio Besser <sergiobesser@hotmail.com>
2022-04-02 17:40:34 -0300

    Se creo formulario de login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import accounts
+from .views import accounts, login, login_view
 
 urlpatterns = [
     path('', accounts, name='accounts'),
+    path('loginview', login_view, name='login_view'),
+    path('login/', login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as django_login, authenticate
 
 def accounts(request):
     return render(request, 'accounts/accounts.html', {})
+
+def login_view(request):
+    return render(request, 'accounts/login_view.html', {})
+
+def login(request):
+    msj = ''
+    if request.method == 'POST':
+        login_form = AuthenticationForm(request, data=request.POST)
+        
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                django_login(request, user)
+                return redirect('login_view')
+            else:
+                msj= 'No se pudo autenticar el usuario'
+        else:
+            msj= 'El formulario no es valido'
+    
+    login_form = AuthenticationForm()
+    return render(request, 'accounts/login_form.html', {'login_form': login_form, 'msj': msj})
