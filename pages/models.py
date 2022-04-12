c6a77ea04e1dfbc3c13c53fa136ea1e9303d4cbc
Sergio Besser <sergiobesser@hotmail.com>
2022-04-12 12:51:21 -0300

    Se agrego ckeditor

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Pages(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=250)
-    body = models.TextField()
+    body = RichTextField()
     author = models.CharField(max_length=50)
     image = models.ImageField(upload_to='pages')
     date = models.DateTimeField()
